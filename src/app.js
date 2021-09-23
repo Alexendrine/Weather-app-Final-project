2d5c0fb14f642565ccbe5fa0c0cbc9e8687c26b1
Alexendrine <alexandrine.herbreteau@laposte.net>
2021-09-23 14:10:23 +0200

    link t° conversion fixed

--- src/app.js
+++ src/app.js
@@ -1,117 +1,3 @@
-// let now = new Date();
-// let time = document.querySelector("#date");
-
-// let hours = now.getHours();
-// if (hours < 10) {
-//   hours = `0${hours}`;
-// }
-// let minutes = now.getMinutes();
-// if (minutes < 10) {
-//   minutes = `0${minutes}`;
-// }
-// let days = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday",
-// ];
-// let day = days[now.getDay()];
-// time.innerHTML = `${day}, ${hours}:${minutes}`;
-
-// function newWeather(response) {
-//   document.querySelector("#city").innerHTML = response.data.name;
-//   document.querySelector("#temperature").innerHTML = Math.round(
-//     response.data.main.temp
-//   );
-//   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-//   document.querySelector("#wind").innerHTML = Math.round(
-//     response.data.wind.speed
-//   );
-//   document.querySelector("#description").innerHTML =
-//     response.data.weather[0].main;
-
-//   document.querySelector(
-//     "#icon"
-//   ).src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-// }
-
-// 0000000
-// // celsiusTemperature = response.data.main.temp;
-// 0000000
-
-// function search(city) {
-//   let apiKey = "8de0142a1472dd581bd2689be42668ec";
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-//   axios.get(apiUrl).then(newWeather);
-// }
-
-// function newCity(event) {
-//   event.preventDefault();
-//   let cityInput = document.querySelector("#city-input");
-//   let city = cityInput.value;
-//   search(city);
-// }
-
-// let searchForm = document.querySelector("#search-form");
-// searchForm.addEventListener("submit", newCity);
-
-// function searchLocation(position) {
-//   let apiKey = "8de0142a1472dd581bd2689be42668ec";
-//   let lat = position.coords.latitude;
-//   let long = position.coords.longitude;
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-//   axios.get(apiUrl).then(newWeather);
-// }
-
-// function getCurrentLocation(event) {
-//   event.preventDefault();
-//   navigator.geolocation.getCurrentPosition(searchLocation);
-// }
-// let currentLocationButton = document.querySelector("#current-location-button");
-// currentLocationButton.addEventListener("click", getCurrentLocation);
-
-// function changeToFahrenheit(event) {
-//   event.preventDefault();
-//   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-//   let temperatureElement = document.querySelector("#temperature");
-//   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-// }
-
-// let fahrenheitLink = document.querySelector("#fahrenheit-link");
-// fahrenheitLink.addEventListener("click", changeToFahrenheit);
-
-// let celsiusTemperature = null;
-
-// 0000000
-// // function changeToFahrenheit(event) {
-// //   event.preventDefault();
-// //   let temperatureElement = document.querySelector("#temperature");
-// //   temperatureElement.innerHTML = 66;
-// // }
-// // let fahrenheitLink = document.querySelector("#fahrenheit-link");
-// // fahrenheitLink.addEventListener("click", changeToFahrenheit);
-
-// // function changeToCelcius(event) {
-// //   event.preventDefault();
-// //   let temperatureElement = document.querySelector("#temperature");
-// //   temperatureElement.innerHTML = 19;
-// // }
-// // let celciusLink = document.querySelector("#celsius-link");
-// // celciusLink.addEventListener("click", changeToCelcius);
-// 00000000
-// search("London");
-
-// let iconElement = document.querySelector("#icon");
-// iconElement.setAttribute(
-//   "src",
-//   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-// );
-// iconElement.setAttribute("alt", response.data.weather[0].description);
-// 000000000
-
 let now = new Date();
 let time = document.querySelector("#date");
 
@@ -192,12 +78,16 @@ currentLocationButton.addEventListener("click", getCurrentLocation);
 function changeToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function changeToCelsius(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
@@ -212,21 +102,5 @@ fahrenheitLink.addEventListener("click", changeToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", changeToCelsius);
-
-// function changeToFahrenheit(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-//   temperatureElement.innerHTML = 66;
-// }
-// let fahrenheitLink = document.querySelector("#fahrenheit-link");
-// fahrenheitLink.addEventListener("click", changeToFahrenheit);
-
-// function changeToCelcius(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-//   temperatureElement.innerHTML = 19;
-// }
-// let celciusLink = document.querySelector("#celsius-link");
-// celciusLink.addEventListener("click", changeToCelcius);
 
 search("London");
