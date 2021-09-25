2fb63d322aed1402d3e612535041f6073ca4a323
Alexendrine <alexandrine.herbreteau@laposte.net>
2021-09-25 18:23:38 +0200

    getting the api

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ let days = [
 let day = days[now.getDay()];
 time.innerHTML = `${day}, ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -50,6 +51,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "8de0142a1472dd581bd2689be42668ec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function newWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let iconElement = document.querySelector("#icon");
@@ -75,6 +82,8 @@ function newWeather(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -134,4 +143,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", changeToCelsius);
 
 search("London");
-displayForecast();
